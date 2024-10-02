ff7a8821b247f7ee6d496bb038ba10146b900f37
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-10-02 10:40:35 +0300

    test player class

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { GameBoard } from "./Gameboard.js";
+import { Player } from "./Player.js";
 
 const gameBoard = new GameBoard();
-
 gameBoard.createBoard();
+const player = new Player("Tom", "X");
+console.log(player.getPlayerSymbol());
+
+console.log(gameBoard.getGameBoardArray());
