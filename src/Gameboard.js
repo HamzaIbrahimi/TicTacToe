594308a96a1cde3c95c3b689c94cadb667a0c423
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-10-02 09:03:49 +0300

    create the gameboard class and render into game_container

---
+++ src/Gameboard.js
@@ -0,0 +1,15 @@
+export class GameBoard {
+  constructor() {
+    this.body = document.body;
+    this.gameContainer = document.querySelector("#game_container");
+    this.gameBoardArray = [];
+  }
+  createBoard() {
+    for (let i = 0; i < 9; i++) {
+      const div = document.createElement("div");
+      div.className = `square_${i + 1} square_box`;
+      this.gameContainer.appendChild(div);
+      this.gameBoardArray.push(div);
+    }
+  }
+}
