76f917b325322cef4024acb9815347a3062988d8
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-10-10 20:50:50 +0200

    Idea scripples and more testing

--- src/main.js
+++ src/main.js
@@ -4,7 +4,19 @@ import { Player } from "./Player.js";
 const gameBoard = new GameBoard();
 gameBoard.createBoard();
 const player = new Player("Tom", "X");
-console.log(player.getPlayerSymbol());
-gameBoard.addSymbolToSquare(0, "X");
+const player1 = new Player("Bob", "O");
+const board = gameBoard.getGameBoardArray();
+// board.forEach((b) => {
+//   b.addEventListener("click", (e) => {
+//     let index = e.currentTarget.className.split(" ")[0].at(-1);
+//     console.log(index);
+//     gameBoard.addSymbolToSquare(index, player.getPlayerSymbol());
+//   });
+// });
+// console.log(board[0].textContent);
 
-gameBoard.clearGameBoardArray();
+const form = document.querySelector("#form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("hello");
+});
