9a63526035825ba88b91d4586c573482aeffc9fe
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-10-15 21:13:15 +0200

    Create class for game flow and add method for player turns

--- src/main.js
+++ src/main.js
@@ -1,22 +1,37 @@
 import { GameBoard } from "./Gameboard.js";
 import { Player } from "./Player.js";
 
-const gameBoard = new GameBoard();
-gameBoard.createBoard();
-const player = new Player("Tom", "X");
-const player1 = new Player("Bob", "O");
-const board = gameBoard.getGameBoardArray();
-// board.forEach((b) => {
-//   b.addEventListener("click", (e) => {
-//     let index = e.currentTarget.className.split(" ")[0].at(-1);
-//     console.log(index);
-//     gameBoard.addSymbolToSquare(index, player.getPlayerSymbol());
-//   });
-// });
-// console.log(board[0].textContent);
+class GameFlow {
+  constructor() {
+    this.gameBoard = new GameBoard();
+    this.gameBoard.createBoard();
+    this.array = this.gameBoard.getGameBoardArray();
+    this.player;
+  }
 
-const form = document.querySelector("#form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("hello");
-});
+  playTurn(player) {
+    this.array.forEach((elem) => {
+      elem.addEventListener("click", (e) => {
+        let index = e.currentTarget.className.split(" ")[0].at(-1);
+        this.gameBoard.addSymbolToSquare(index, player.getSymbol());
+      });
+    });
+  }
+}
+const player1 = new Player("Tom", "X");
+const player2 = new Player("Bob", "O");
+
+const game = new GameFlow();
+let turn = 1;
+
+if (turn === 1) {
+  console.log(turn);
+  game.playTurn(player1);
+  turn = 0;
+} else if (turn === 0) {
+  console.log(turn);
+  game.playTurn(player2);
+  turn = 1;
+}
+
+// game.gameBoard.clearGameBoardArray();
