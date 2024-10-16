d153abd1a6c9eb477e2db69ff880d0c0b61ff618
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-10-16 09:40:55 +0200

    Finally able to change symbol on click but requires some refining

--- src/main.js
+++ src/main.js
@@ -11,10 +11,14 @@ class GameFlow {
 
   playTurn(player) {
     this.array.forEach((elem) => {
-      elem.addEventListener("click", (e) => {
-        let index = e.currentTarget.className.split(" ")[0].at(-1);
-        this.gameBoard.addSymbolToSquare(index, player.getSymbol());
-      });
+      elem.addEventListener(
+        "click",
+        (e) => {
+          let index = e.currentTarget.className.split(" ")[0].at(-1);
+          this.gameBoard.addSymbolToSquare(index, player.getSymbol());
+        },
+        { once: true }
+      );
     });
   }
 }
@@ -22,16 +26,21 @@ const player1 = new Player("Tom", "X");
 const player2 = new Player("Bob", "O");
 
 const game = new GameFlow();
-let turn = 1;
 
-if (turn === 1) {
-  console.log(turn);
-  game.playTurn(player1);
-  turn = 0;
-} else if (turn === 0) {
-  console.log(turn);
-  game.playTurn(player2);
-  turn = 1;
-}
-
+game.array.forEach((elem) => {
+  elem.addEventListener(
+    "click",
+    (e) => {
+      let place = e.currentTarget.className.split(" ")[0].at(-1);
+      if (place % 2 == 0) {
+        game.playTurn(player2);
+        console.log(place);
+      } else {
+        console.log(place);
+        game.playTurn(player1);
+      }
+    },
+    { once: true }
+  );
+});
 // game.gameBoard.clearGameBoardArray();
