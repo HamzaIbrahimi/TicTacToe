ad3f3d209f6df64be6a3da874d949d84666433fc
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-10-19 21:37:45 +0200

    Successfully changes symbol on click

--- src/main.js
+++ src/main.js
@@ -6,41 +6,32 @@ class GameFlow {
     this.gameBoard = new GameBoard();
     this.gameBoard.createBoard();
     this.array = this.gameBoard.getGameBoardArray();
-    this.player;
+    this.gameContainer = this.gameBoard.gameContainer;
   }
 
-  playTurn(player) {
-    this.array.forEach((elem) => {
-      elem.addEventListener(
-        "click",
-        (e) => {
-          let index = e.currentTarget.className.split(" ")[0].at(-1);
-          this.gameBoard.addSymbolToSquare(index, player.getSymbol());
-        },
-        { once: true }
-      );
-    });
+  playTurn(player, index) {
+    this.gameBoard.addSymbolToSquare(index, player.getSymbol());
   }
 }
 const player1 = new Player("Tom", "X");
 const player2 = new Player("Bob", "O");
 
+//Issue: The turns are only taking into consideration each cell item, so the switching does
+//indeed occur but we need to carry this outward to all elements of the array
 const game = new GameFlow();
 
-game.array.forEach((elem) => {
-  elem.addEventListener(
-    "click",
-    (e) => {
-      let place = e.currentTarget.className.split(" ")[0].at(-1);
-      if (place % 2 == 0) {
-        game.playTurn(player2);
-        console.log(place);
-      } else {
-        console.log(place);
-        game.playTurn(player1);
-      }
-    },
-    { once: true }
-  );
+let turn = 0;
+game.gameContainer.addEventListener("click", (e) => {
+  if (e.target && e.target.className.includes("square_box")) {
+    let index = e.target.className.split(" ")[0].at(-1);
+    if (turn % 2 == 0 && e.target.textContent.length == 0) {
+      turn++;
+      game.playTurn(player1, index);
+    } else if (turn % 2 != 0 && e.target.textContent.length == 0) {
+      turn++;
+      game.playTurn(player2, index);
+    } else {
+      return;
+    }
+  }
 });
-// game.gameBoard.clearGameBoardArray();
